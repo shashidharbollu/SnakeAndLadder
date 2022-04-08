fa7577fee61550b9f6ad2da143ca73aff61e2429
shashidharbollu <shashidharbollu77@gmail.com>
2022-04-08 18:57:27 +0530

    [shashidhar] Add : snake and ladder game to play with single player at position 0 added

---
+++ SnakeAndLadderGAME/Ladder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGAME
+{
+    public class ladder
+
+    {
+        const int position = 0;
+        public int dieRoll()
+        {
+            return position;
+        }
+
+    }
+}
---
+++ SnakeAndLadderGAME/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SnakeAndLadderGAME;
+public class program
+{
+    public static void Main(String[] args)
+    {
+        ladder game = new ladder();
+        int start = game.dieRoll();
+        Console.WriteLine("Welcome in the Game, Your Position is: " + "****" + start + "****");
+    }
+}
